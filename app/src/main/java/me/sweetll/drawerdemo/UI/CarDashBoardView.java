122bf4a2ef3295615c88b6b66d4b72e2f79f3e0d
blackbbc <505968815@qq.com>
2015-08-13 20:35:32 +0800

    Initialize Car Dashboard View

---
+++ app/src/main/java/me/sweetll/drawerdemo/UI/CarDashBoardView.java
@@ -0,0 +1,15 @@
+package me.sweetll.drawerdemo.UI;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by sweet on 15-8-13.
+ */
+public class CarDashBoardView extends View {
+
+    public CarDashBoardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
