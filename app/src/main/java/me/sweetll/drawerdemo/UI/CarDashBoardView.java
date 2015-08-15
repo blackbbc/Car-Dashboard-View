eaf616bc875c35af7575ad02e1a34aea06db8cd5
blackbbc <505968815@qq.com>
2015-08-15 21:56:36 +0800

    update 3-rd party library

--- app/src/main/java/me/sweetll/drawerdemo/UI/CarDashBoardView.java
+++ app/src/main/java/me/sweetll/drawerdemo/UI/CarDashBoardView.java
@@ -1,6 +1,10 @@
 package me.sweetll.drawerdemo.UI;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -8,8 +12,34 @@ import android.view.View;
  * Created by sweet on 15-8-13.
  */
 public class CarDashBoardView extends View {
+    private Paint paint;
+    RectF oval = new RectF();
+
+    private void init() {
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(3.0f);
+        paint.setStyle(Paint.Style.STROKE);
+    }
 
     public CarDashBoardView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        oval.left = 100;
+        oval.top = 100;
+        oval.right = 400;
+        oval.bottom = 300;
+        canvas.drawArc(oval, 200, 135, true, paint);
+
+        oval.left = 100;
+        oval.top = 400;
+        oval.right = 400;
+        oval.bottom = 700;
+        canvas.drawArc(oval, 200, 135, true, paint);
     }
 }
